d00baa7cef5e4db74ee24f5d468b40582315727d
James Licarte <james.licarte59gmail.com>
2025-01-28 03:07:50 +0800

    add showing of user details

--- src/app/manage/[id]/page.tsx
+++ src/app/manage/[id]/page.tsx
@@ -1,7 +1,42 @@
+import prisma from '@/lib/db'
+import { Card, Descriptions } from 'antd'
+import { DescriptionsItemType } from 'antd/es/descriptions'
 import React from 'react'
 
-const ManageDetails = () => {
-  return <div>ManageDetails</div>
+const ManageDetails = async ({ params }: { params: { id: string } }) => {
+  const employee = await prisma.employee.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+
+  if (!employee) {
+    return <Card title="User details">User not found</Card>
+  }
+
+  const items: DescriptionsItemType[] = [
+    {
+      key: 'first_name',
+      label: 'First name',
+      children: employee.first_name,
+    },
+    {
+      key: 'last_name',
+      label: 'Last name',
+      children: employee.last_name,
+    },
+    {
+      key: 'createdAt',
+      label: 'Created at',
+      children: employee.createdAt.toDateString(),
+    },
+  ]
+
+  return (
+    <Card title="User details">
+      <Descriptions layout="vertical" items={items} />
+    </Card>
+  )
 }
 
 export default ManageDetails
